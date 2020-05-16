415c307b1be983425409ce813af1d73eae6a7bf9
Steve Peters <stpeters@collegeboard.org>
2020-05-15 21:44:26 -0500

    Initial checkin of webview video test

---
+++ app/src/main/java/org/collegeboard/webvideotest/MainActivity.kt
@@ -0,0 +1,48 @@
+package org.collegeboard.webvideotest
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.webkit.PermissionRequest
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.collections.ArrayList
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val myWebView = WebView(this)
+
+        val webSettings = myWebView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.allowFileAccessFromFileURLs = true
+        webSettings.allowUniversalAccessFromFileURLs = true
+        webSettings.mediaPlaybackRequiresUserGesture = false
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val hasCameraPermission = checkSelfPermission(Manifest.permission.CAMERA)
+            val permissions: MutableList<String> = ArrayList()
+            if (hasCameraPermission != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.CAMERA)
+            }
+            if (permissions.isNotEmpty()) {
+                requestPermissions(permissions.toTypedArray(), 111)
+            }
+        }
+
+        myWebView.webChromeClient = object : WebChromeClient() {
+            override fun onPermissionRequest(request: PermissionRequest) {
+                    request.grant(request.resources)
+            }
+        }
+
+        setContentView(myWebView)
+        myWebView.loadUrl("https://webrtc.github.io/samples/src/content/getusermedia/gum/")
+    }
+
+
+}
